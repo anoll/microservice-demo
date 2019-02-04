0b6a76cc1c9b07e6f6b8fce604df839209c03cd1
Albert Noll <noll.albert@gmail.com>
2019-02-04 15:54:39 +0100

    added swagger ui

--- src/main/java/com/oracle/microservice/demo/AddressBookController.java
+++ src/main/java/com/oracle/microservice/demo/AddressBookController.java
@@ -1,10 +1,12 @@
 package com.oracle.microservice.demo;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
+@Api(value="TO CHANGE", description="TO CHANGE")
 @RestController
 public class AddressBookController {
     private AddressRepository repository;
--- src/main/java/com/oracle/microservice/demo/SpringFoxConfig.java
+++ src/main/java/com/oracle/microservice/demo/SpringFoxConfig.java
@@ -10,11 +10,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SpringFoxConfig {
     @Bean
-    public Docket api() {
+    public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
